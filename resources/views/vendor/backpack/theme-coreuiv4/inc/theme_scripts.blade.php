e1b7c28351e50c405c26c74c61c9ce7d0fed331b
Ubuntu <ubuntu@vps-1ec0000b.vps.ovh.net>
2026-03-19 21:24:18 +0000

    work theme

---
+++ resources/views/vendor/backpack/theme-coreuiv4/inc/theme_scripts.blade.php
@@ -0,0 +1,3 @@
+@basset('https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js')
+@basset('https://unpkg.com/@coreui/coreui@4.2.6/dist/js/coreui.min.js')
+@basset('https://unpkg.com/simplebar@6.2.7/dist/simplebar.min.js')
---
+++ resources/views/vendor/backpack/theme-coreuiv4/inc/theme_styles.blade.php
@@ -0,0 +1,5 @@
+@basset('https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.6/dist/css/coreui.min.css')
+@basset('https://cdn.jsdelivr.net/npm/simplebar@6.2.7/dist/simplebar.min.css')
+
+{{-- Custom Backpack Rules --}}
+@basset(base_path('vendor/backpack/theme-coreuiv4/resources/assets/css/coreui4.css'))
